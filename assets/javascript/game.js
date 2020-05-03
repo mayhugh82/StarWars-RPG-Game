68f6e9533068c8d2f2f01017469f8cdcaac80e06
Diana Mayhugh <dianamayhugh@gmail.com>
2020-05-03 13:34:02 -0400

    if statement character obj property rendered

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,29 +4,44 @@ $(document).ready(function(){
         name: "Xena",
         health: 100,
         attack: 14,
-        imageUrl: "assets/images/Xena.jpg",
+        imageUrl: "..assets/images/Xena.jpg",
         enemyAttackBack: 15,
       },
       Gabrielle: {
         name: "Gabrielle",
         health: 120,
         attack: 8,
-        imageUrl: "assets/images/Gabrielle.jpg",
+        imageUrl: "..assets/images/Gabrielle.jpg",
         enemyAttackBack: 15,
       },
       Callisto: {
         name: "Callisto",
         health: 150,
         attack: 8,
-        imageUrl: "assets/images/callisto.jpeg",
+        imageUrl: "..assets/images/callisto.jpeg",
         enemyAttackBack: 15,
       },
       Ares: {
         name: "Ares",
         health: 180,
         attack: 7,
-        imageUrl: "assets/images/ares.jpg",
+        imageUrl: "..assets/images/ares.jpg",
         enemyAttackBack: 15,
       }
     };
+    console.log(characters);
+    //This function will render a character card to the page.
+    //The character rendered and the area they are rendered to.
+    //This function handles the rendering of the characters based on which area they are to be rendered in.
+    var renderCharacters = function(charObj, areaRender) {
+        if (arearender === "#characters-section") {
+            $(areadrender).empty();
+            for (var key in charObj) {
+                if (charObj.hasOwnProperty(key)) {
+                    CanvasRenderingContext2D(charObj[key], areaRender);
+                }
+            }
+        }
+
+    }
 });
