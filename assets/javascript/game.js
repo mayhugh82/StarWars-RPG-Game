37a73629cce662731d42539725e5496d427711ef
Diana Mayhugh <dianamayhugh@gmail.com>
2020-05-03 13:23:53 -0400

    characters var in js

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    var characters = {
+      Xena: {
+        name: "Xena",
+        health: 100,
+        attack: 14,
+        imageUrl: "assets/images/Xena.jpg",
+        enemyAttackBack: 15,
+      },
+      Gabrielle: {
+        name: "Gabrielle",
+        health: 120,
+        attack: 8,
+        imageUrl: "assets/images/Gabrielle.jpg",
+        enemyAttackBack: 15,
+      },
+      Callisto: {
+        name: "Callisto",
+        health: 150,
+        attack: 8,
+        imageUrl: "assets/images/callisto.jpeg",
+        enemyAttackBack: 15,
+      },
+      Ares: {
+        name: "Ares",
+        health: 180,
+        attack: 7,
+        imageUrl: "assets/images/ares.jpg",
+        enemyAttackBack: 15,
+      }
+    };
+});
